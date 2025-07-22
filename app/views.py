f4c11acfd95b1a31c95b8187de36dfcd5dd32782
Gabriel Vieira <nox030705@gmail.com>
2025-07-21 23:54:04 -0400

    feat: added maximum iterations

--- app/views.py
+++ app/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+count = 1
+
+def index(_):
+    global count
+    if count > 5:
+        # Max iterations reached
+        return HttpResponse(b"STOP!!")
+    count += 1
+    return HttpResponse(bytes(str(count), 'utf-8'))
